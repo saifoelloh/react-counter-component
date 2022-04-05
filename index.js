42178ee00603f668333a8fe45576ca0bdf53977a
saifoelloh <saifoelloh@gmail.com>
2022-04-05 12:15:35 +0700

    feat: initialize

---
+++ index.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>increment</button>
+      <button onClick={() => setCount(count - 1)}>decrement</button>
+    </div>
+  );
+};
+
+const CustomCounter = (initialValue = 0, stepper = 1) => {
+  const [count, setCount] = React.useState(initialValue);
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + stepper)}>increment</button>
+      <button onClick={() => setCount(count - stepper)}>decrement</button>
+    </div>
+  );
+};
+
+export { Counter, CustomCounter };
+
+export default Counter;
